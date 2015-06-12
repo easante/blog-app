c0a6da9e6bf2e59f91e0ba17cb0a9ed09de20bd8
Emmanuel Asante <emmanuela.asante@gmail.com>
2015-06-12 13:37:42 -0600

    Implement showing an article

---
+++ spec/features/showing_article_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+RSpec.feature "Showing an Article" do
+
+  before do
+    @article = Article.create(title: "The first article",
+                body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+  end
+
+  scenario "A user lists all articles" do
+    visit "/"
+
+    click_link @article.title
+
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+  end
+
+end
