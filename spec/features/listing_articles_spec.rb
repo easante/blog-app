99b4a13d8b3cff2648cc7fd344ab8ac44045fe65
Emmanuel Asante <emmanuela.asante@gmail.com>
2015-06-14 21:10:38 -0600

    Hide links from non-signed in users

--- spec/features/listing_articles_spec.rb
+++ spec/features/listing_articles_spec.rb
@@ -3,13 +3,16 @@ require "rails_helper"
 RSpec.feature "Listing Articles" do
 
   before do
+    @john = User.create(email: "john@example.com", password: "password")
+    # login_as(@john)
+
     @article1 = Article.create(title: "The first article",
                 body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
     @article2 = Article.create(title: "The second article",
                 body: "Pellentesque ac ligula in tellus feugiat iaculis.")
   end
 
-  scenario "A user lists all articles" do
+  scenario "Non-signed in users do not see 'New Article' link" do
     visit "/"
 
     expect(page).to have_content(@article1.title)
@@ -18,6 +21,14 @@ RSpec.feature "Listing Articles" do
     expect(page).to have_content(@article2.body)
     expect(page).to have_link(@article1.title)
     expect(page).to have_link(@article2.title)
+    expect(page).not_to have_link("New Article")
+  end
+
+  scenario "Signed in users see all articles and link" do
+    login_as(@john)
+
+    visit "/"
+    expect(page).to have_link("New Article")
   end
 
 end
--- spec/features/showing_article_spec.rb
+++ spec/features/showing_article_spec.rb
@@ -3,11 +3,13 @@ require "rails_helper"
 RSpec.feature "Showing an Article" do
 
   before do
+    @john = User.create(email: "john@example.com", password: "password")
     @article = Article.create(title: "The first article",
-                body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+                body: "Lorem ipsum dolor sit amet.",
+                user: @john)
   end
 
-  scenario "A user lists all articles" do
+  scenario "A non-signed in user does not see Edit Delete Links" do
     visit "/"
 
     click_link @article.title
@@ -15,6 +17,19 @@ RSpec.feature "Showing an Article" do
     expect(page).to have_content(@article.title)
     expect(page).to have_content(@article.body)
     expect(current_path).to eq(article_path(@article))
+
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
+  end
+
+  scenario "A signed in user sees both links" do
+    login_as(@john)
+
+    visit "/"
+    click_link @article.title
+    
+    expect(page).to have_link("Edit Article")
+    expect(page).to have_link("Delete Article")
   end
 
 end
