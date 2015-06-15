941ea1ba1eea59c3b711da8d5aa8c40a3a4c8e95
Emmanuel Asante <emmanuela.asante@gmail.com>
2015-06-14 21:34:48 -0600

    Restrict editing and modification access to owners

--- spec/features/showing_article_spec.rb
+++ spec/features/showing_article_spec.rb
@@ -3,7 +3,10 @@ require "rails_helper"
 RSpec.feature "Showing an Article" do
 
   before do
-    @john = User.create(email: "john@example.com", password: "password")
+    @john = User.create(email: "john@example.com",
+                        password: "password")
+    @fred = User.create(email: "fred@example.com",
+                        password: "password")
     @article = Article.create(title: "The first article",
                 body: "Lorem ipsum dolor sit amet.",
                 user: @john)
@@ -22,14 +25,23 @@ RSpec.feature "Showing an Article" do
     expect(page).not_to have_link("Delete Article")
   end
 
+  scenario "A non-owner signed in cannot see both links" do
+    login_as(@fred)
+
+    visit "/"
+    click_link @article.title
+
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
+  end
+
   scenario "A signed in user sees both links" do
     login_as(@john)
 
     visit "/"
     click_link @article.title
-    
+
     expect(page).to have_link("Edit Article")
     expect(page).to have_link("Delete Article")
   end
-
 end
